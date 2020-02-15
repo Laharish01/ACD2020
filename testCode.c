894e47bed99321c34cb72048b8a213861713d66e
harsh <harsh.aayush.r1@gmail.com>
2020-02-15 19:24:20 +0530

    Ignores comments

--- testCode.c
+++ testCode.c
@@ -1,11 +1,19 @@
+/*cdavsdvda
+faefea
+feafea*/
 #include <stdio.h>
 
 int main(){
     int a = 10;
-    printf("This is a string");
+//gdyrr87tut
+
         char c = 'b';
+//fgeg
+//fsegsdgsdh
+
         char c = 'b';
-    if(a>b){
+
+        if(a>b){
         c=3;
     }  
 }
