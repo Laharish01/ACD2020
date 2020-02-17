bd92f756a536ca08dc71e17ebd127617a7dd0f83
harsh <harsh.aayush.r1@gmail.com>
2020-02-17 13:16:44 +0530

    Ignores comments

--- testCode.c
+++ testCode.c
@@ -1,18 +1,17 @@
-/*cdavsdvda
-faefea
-feafea*/
+
 #include <stdio.h>
 
 int main(){
     int a = 10;
 //gdyrr87tut
-
         char c = 'b';
 //fgeg
 //fsegsdgsdh
 
         char c = 'b';
-
+/*cdavsdvda
+faefea
+feafea*/
         if(a>b){
         c=3;
     }  
